945c20ecbb328e2360ba412a7f189d0a3d6fddb8
gausszhou <gausszhou@qq.com>
2025-03-25 22:40:39 +0800

    chore: 更新 roms 目录结构

--- roms/gen-info.js
+++ roms/gen-info.js
@@ -1,16 +1,22 @@
-const fs = require('fs');
+const fs = require("fs");
+const path = require("path");
 
+const dir = fs.readdirSync(__dirname);
+const result = [];
 
-const dir = fs.readdirSync(__dirname)
-
-let result = [];
-Array.from(dir).forEach(filePath => {
-  if (filePath.includes('.gba')) {
-    const matches = filePath.match(/\w+\s(?<name>[^\.]+)\.gba/);
-    const path = matches[0];
-    const name = matches[1];
-    result.push({name, path})
+Array.from(dir).forEach((filePath) => {
+  const fileOrFolder = path.join(__dirname, filePath);
+  if (fs.statSync(fileOrFolder).isDirectory()) {
+    const dir1 = fs.readdirSync(fileOrFolder);
+    Array.from(dir1).forEach((item) => {
+      if (item.includes(".zip") || item.includes(".gba")) {
+        const matches = item.match(/\w+\s(?<name>[^\.]+)\.(zip|gba)/);     
+        const path = filePath + '/' + matches[0];
+        const name = matches[1];
+        result.push({ name, path });
+      }
+    });
   }
-})
+});
 
-fs.writeFileSync(__dirname + '/info.json', JSON.stringify(result, null, 2))
+fs.writeFileSync(__dirname + "/info.json", JSON.stringify(result, null, 2));
